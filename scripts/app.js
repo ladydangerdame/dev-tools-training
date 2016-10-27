30cd992635c573d3a9ca42fccbdd7fc9108da399
ladydangerdame <turbofemme@gmail.com>
2016-10-26 20:49:45 -0700

    fixed link display bug

--- scripts/app.js
+++ scripts/app.js
@@ -14,14 +14,15 @@ $(document).ready(function(){
 
 	// the form redirects when it should just update the count of "E"s
 	$('form').on('submit', function(e){
+		e.preventDefault();
 		console.log('form submitted');
 		var input = $('input').eq(0).val();
 
 		// should count occurrences of the letter "E"
 		// and update the display. instead always shows 0
-		for (var i = 0; i < 10; i++){
+		var count = 0;
+		for (var i = 0; input.length; i++){
 			// this loop doesn't find both es in  "supercalifragilisticexpialidocious"
-			var count = 0;
 			if (input[i] === "e"){
 				count = count +1;
 			}
@@ -30,7 +31,8 @@ $(document).ready(function(){
 	});
 
 
-	// all the links alert 5 :(
+	// all the links alert 5 :
+
 	function addLinks () {
 	    var $link;
 	    var $ul = $('ul');
@@ -45,4 +47,4 @@ $(document).ready(function(){
     }
 
     addLinks();
-};
+});
